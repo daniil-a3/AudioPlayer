0da787f66ae4730e96b09eb324f018186f35238e
daniil-a3 <daniila3@educbe.ca>
2025-05-26 13:40:16 -0600

    File playback now functional
    Only .wav files work, despite what the file opening dialogue says. I guess I lied a lil :P

--- src/AudioPlayer.java
+++ src/AudioPlayer.java
@@ -1,5 +1,6 @@
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.filechooser.FileFilter;
@@ -7,18 +8,31 @@ import javax.swing.filechooser.FileFilter;
 import java.awt.Toolkit;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+
 import java.io.File;
+import java.io.IOException;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineEvent;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 
 public class AudioPlayer extends JFrame {
 	boolean isMuted=false;
-	File currentFile=null;
+	int volume=100;
+	
+	File currentFile;
+	Clip clip;
+	AudioInputStream audioInputStream;
+	
 	JLabel currentTrack=new JLabel("No file selected");
 	JButton backButton, playButton, stopButton, nextButton,
 	muteButton, openButton;
-	int volume=100;
 	
 	public static void main(String[] args) {
 		AudioPlayer frame=new AudioPlayer();
@@ -79,7 +93,12 @@ public class AudioPlayer extends JFrame {
 		openButton.setBounds(360, 40, 50, 50);
 		openButton.addMouseListener(new MouseAdapter() {
 			public void mouseClicked(MouseEvent e) {
-				openButton_mouseClicked(e);
+				try {
+					openButton_mouseClicked(e);
+				} catch (UnsupportedAudioFileException
+						| IOException e1) {
+					e1.printStackTrace();
+				}
 			}
 		});
 		
@@ -97,7 +116,19 @@ public class AudioPlayer extends JFrame {
 	}
 	
 	private void playButton_mouseClicked(MouseEvent e) {
-		Toolkit.getDefaultToolkit().beep();
+		try {
+			clip.start();
+			clip.addLineListener(e1 -> {
+				if (e1.getType() == LineEvent.Type.STOP) {
+					clip.stop();
+					clip.close();
+					loadFile();
+				}
+			});
+		} catch (Exception e1) {
+			Toolkit.getDefaultToolkit().beep();
+			System.err.println(e1);
+		}
 	}
 	
 	private void stopButton_mouseClicked(MouseEvent e) {
@@ -118,30 +149,72 @@ public class AudioPlayer extends JFrame {
 		}
 	}
 	
-	private void openButton_mouseClicked(MouseEvent e) {
+	private void openButton_mouseClicked(MouseEvent e) 
+	throws UnsupportedAudioFileException, IOException {
 		JFileChooser fileChooser=new JFileChooser();
 		fileChooser.setAcceptAllFileFilterUsed(false);
 		fileChooser.setFileFilter(new FileFilter() {
 
-			   public String getDescription() {
-			       return "All audio files (*.wav, *.mp3)";
-			   }
+				public String getDescription() {
+					return "All audio files (*.wav, *.mp3, *.aac)";
+				}
 
-			   public boolean accept(File f) {
-			       if (f.isDirectory()) {
-			           return true;
-			       } else {
-			           String filename = f.getName().toLowerCase();
-			           return filename.endsWith(".wav") || filename.endsWith(".mp3") ;
-			       }
-			   }
+				public boolean accept(File f) {
+					if (f.isDirectory()) {
+						return true;
+					} else {
+						String filename = f.getName().toLowerCase();
+						return filename.endsWith(".wav")
+						|| filename.endsWith(".mp3") 
+						|| filename.endsWith(".aac") ;
+					}
+				}
 			});
 		int result=fileChooser.showOpenDialog(fileChooser);
 		if (result==JFileChooser.APPROVE_OPTION) {
-		    File selectedFile=fileChooser.getSelectedFile();
-		    System.out.println("Selected file: "+selectedFile.getAbsolutePath());
-		    currentTrack.setText(selectedFile.getName());
-		    currentFile=selectedFile;
+			File selectedFile=fileChooser.getSelectedFile();
+			System.out.println("Selected file: "+selectedFile.getAbsolutePath());
+			currentTrack.setText(selectedFile.getName());
+			currentFile=selectedFile;
 		}
+		
+		loadFile();
+	}
+	
+	private void loadFile() {
+		try {
+			System.out.println(currentFile.toString());
+			audioInputStream=AudioSystem.getAudioInputStream
+					(new File(currentFile.toString())
+							.getAbsoluteFile());
+			clip=AudioSystem.getClip();
+			clip.open(audioInputStream);
+		} catch (NullPointerException e1) {
+			System.out.println("No file chosen");
+		} catch (UnsupportedAudioFileException e1) {
+			showErrorMessage(String.format
+			("File extension \"%s\" is currently not supported.",
+			getFileExt(currentFile.toString())),
+			"Unsupported File Type");
+		} catch (Exception e1) {
+			Toolkit.getDefaultToolkit().beep();
+			System.err.println(e1);
+		}
+	}
+	
+	private String getFileExt(String filename) {
+		if (filename==null) {
+			return null;
+		}
+		int dotIndex=filename.lastIndexOf(".");
+		if (dotIndex>=0) {
+			return filename.substring(dotIndex+1);
+		}
+		return "";
+	}
+	
+	public static void showErrorMessage(String message, String title) {
+		JOptionPane.showMessageDialog(null, message, title,
+			JOptionPane.ERROR_MESSAGE);
 	}
 }
